46fad3ff4a23ad33fbab5412887456d3caee08c5
“stefanhans” <uni@stefan-hans.de>
2020-04-12 16:53:32 +0200

    Ready to retry Cloud Run

--- http-server/main.go
+++ http-server/main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
 
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
+
 func main() {
+	ipv4 := GetLocalIP()
+	fmt.Printf("ipv4: %s\n", ipv4)
+
 	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(fmt.Sprintf("%s:8080", ipv4), nil)
+	if err != nil {
+		fmt.Printf("failed to listen and serve: %v\n", err)
+	}
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
